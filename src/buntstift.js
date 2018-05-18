a67788b1a81eeb73fbdfacb200aaf5656e0b4b78
Golo Roden <golo.roden@thenativeweb.io>
2018-05-18 11:22:26 +0200

    Clean up code.

--- src/buntstift.js
+++ src/buntstift.js
@@ -1,291 +1,238 @@
 'use strict';
 
-const util = require('util');
-
 const chalk = require('chalk'),
       Spinner = require('node-spinner');
 
 const is = require('./is'),
-      pad = require('./pad');
+      pad = require('./pad'),
+      unicode = require('./unicode');
 
-let unicode = require('./unicode')[is.utf() ? 'utf8' : 'ascii'];
+let characters = unicode[is.utf() ? 'utf8' : 'ascii'];
 
 let interval,
     stopSpinner;
 
-const buntstift = {};
-
-buntstift.forceColor = function () {
-  chalk.enabled = true;
-};
-
-buntstift.noColor = function () {
-  chalk.enabled = false;
-};
-
-buntstift.forceUtf = function () {
-  /* eslint-disable global-require */
-  unicode = require('./unicode').utf8;
-  /* eslint-enable global-require */
-};
-
-buntstift.noUtf = function () {
-  /* eslint-disable global-require */
-  unicode = require('./unicode').ascii;
-  /* eslint-enable global-require */
-};
-
-buntstift.error = function (message, options = {}) {
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.error(chalk.red.bold(util.format('%s %s', options.prefix || unicode.crossMark, String(message))));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.warn = function (message, options = {}) {
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.error(chalk.yellow.bold(util.format('%s %s', options.prefix || unicode.rightPointingPointer, String(message))));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.success = function (message, options = {}) {
-  if (is.quiet()) {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.log(chalk.green.bold(util.format('%s %s', options.prefix || unicode.checkMark, String(message))));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.info = function (message, options = {}) {
-  if (is.quiet()) {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.log(chalk.white(util.format('%s %s', options.prefix || ' ', String(message))));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.verbose = function (message, options = {}) {
-  if (is.quiet() || !is.verbose()) {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.log(chalk.gray(util.format('%s %s', options.prefix || ' ', String(message))));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.header = function (headline, options = {}) {
-  options.prefix = options.prefix || unicode.rightPointingPointer;
-
-  buntstift.line();
-  buntstift.info(headline, options);
-  buntstift.line();
-
-  return buntstift;
-};
-
-buntstift.passThrough = function (message, options = {}) {
-  options.target = options.target || 'stdout';
-
-  if (is.quiet() && options.target === 'stdout') {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  process[options.target].write(`${options.prefix || ' '} ${String(message)}`);
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.list = function (message, options = {}) {
-  options.indent = options.indent || 0;
-  options.prefix = options.prefix || unicode.multiplicationDot;
-
-  const width = options.indent * (options.prefix.length + 1);
-
-  options.prefix = new Array(width + 1).join(' ') + options.prefix;
-
-  buntstift.info(message, options);
-
-  return buntstift;
-};
-
-buntstift.table = function (rows) {
-  if (!rows) {
-    throw new Error('Rows are missing.');
-  }
-
-  const widths = [];
-
-  rows.forEach(row => {
-    row.forEach((value, columnIndex) => {
-      widths[columnIndex] = Math.max(widths[columnIndex] || 0, String(value).length);
-    });
-  });
-
-  rows.forEach(row => {
-    const line = [];
-
-    if (row.length > 0) {
-      row.forEach((value, columnIndex) => {
-        line.push(pad(value, widths[columnIndex]));
-      });
-    } else {
-      widths.forEach(width => {
-        line.push(new Array(width + 1).join(unicode.boxDrawingsLightHorizontal));
-      });
-    }
-
-    buntstift.info(line.join('  '));
-  });
-
-  return buntstift;
-};
-
-buntstift.line = function () {
-  if (is.quiet()) {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.log(chalk.gray('\u2500'.repeat(process.stdout.columns || 80)));
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.newLine = function () {
-  if (is.quiet()) {
-    return buntstift;
-  }
-
-  let spinnerNeedsRestart = false;
-
-  if (stopSpinner) {
-    stopSpinner();
-    spinnerNeedsRestart = true;
-  }
-
-  console.log();
-
-  if (spinnerNeedsRestart) {
-    buntstift.wait();
-  }
-
-  return buntstift;
-};
-
-buntstift.wait = function () {
-  if (is.quiet() || !is.interactiveMode()) {
-    return function () {
-      // Intentionally left blank.
+const decorators = {
+  pauseSpinner (fn) {
+    return function (...args) {
+      let spinnerNeedsRestart = false;
+
+      if (stopSpinner) {
+        stopSpinner();
+        spinnerNeedsRestart = true;
+      }
+
+      const result = fn(...args);
+
+      if (spinnerNeedsRestart) {
+        /* eslint-disable no-use-before-define */
+        buntstift.wait();
+        /* eslint-enable no-use-before-define */
+      }
+
+      return result;
+    };
+  },
+
+  skipIfQuiet (fn) {
+    return function (...args) {
+      if (is.quiet()) {
+        /* eslint-disable no-use-before-define */
+        return buntstift;
+        /* eslint-enable no-use-before-define */
+      }
+
+      const result = fn(...args);
+
+      return result;
+    };
+  },
+
+  skipIfNotVerbose (fn) {
+    return function (...args) {
+      if (!is.verbose()) {
+        /* eslint-disable no-use-before-define */
+        return buntstift;
+        /* eslint-enable no-use-before-define */
+      }
+
+      const result = fn(...args);
+
+      return result;
     };
   }
-
-  if (stopSpinner) {
-    return;
-  }
-
-  const spinner = new Spinner();
-
-  interval = setInterval(() => {
-    process.stderr.write(`\r${spinner.next()}`);
-  }, 50);
-
-  stopSpinner = function () {
-    stopSpinner = undefined;
-    process.stderr.write('\r');
-    clearInterval(interval);
-  };
-
-  return stopSpinner;
 };
 
-buntstift.exit = function (code) {
-  if (stopSpinner) {
-    stopSpinner();
-  }
+const buntstift = {
+  forceColor () {
+    chalk.enabled = true;
+  },
 
-  /* eslint-disable no-process-exit */
-  process.exit(code || 0);
-  /* eslint-enable no-process-exit */
+  noColor () {
+    chalk.enabled = false;
+  },
+
+  forceUtf () {
+    characters = unicode.utf8;
+  },
+
+  noUtf () {
+    characters = unicode.ascii;
+  },
+
+  error: decorators.pauseSpinner(
+    (message, { prefix = characters.crossMark } = {}) => {
+      console.error(chalk.red.bold(`${prefix} ${String(message)}`));
+
+      return buntstift;
+    }
+  ),
+
+  warn: decorators.pauseSpinner(
+    (message, { prefix = characters.rightPointingPointer } = {}) => {
+      console.error(chalk.yellow.bold(`${prefix} ${String(message)}`));
+
+      return buntstift;
+    }
+  ),
+
+  success: decorators.skipIfQuiet(decorators.pauseSpinner(
+    (message, { prefix = characters.checkMark } = {}) => {
+      console.log(chalk.green.bold(`${prefix} ${String(message)}`));
+
+      return buntstift;
+    }
+  )),
+
+  info: decorators.skipIfQuiet(decorators.pauseSpinner(
+    (message, { prefix = ' ' } = {}) => {
+      console.log(chalk.white(`${prefix} ${String(message)}`));
+
+      return buntstift;
+    }
+  )),
+
+  verbose: decorators.skipIfQuiet(decorators.skipIfNotVerbose(decorators.pauseSpinner(
+    (message, { prefix = ' ' } = {}) => {
+      console.log(chalk.gray(`${prefix} ${String(message)}`));
+
+      return buntstift;
+    }
+  ))),
+
+  line: decorators.skipIfQuiet(decorators.pauseSpinner(
+    () => {
+      console.log(chalk.gray('\u2500'.repeat(process.stdout.columns || 80)));
+
+      return buntstift;
+    }
+  )),
+
+  header (headline, { prefix = characters.rightPointingPointer } = {}) {
+    buntstift.line();
+    buntstift.info(headline, { prefix });
+    buntstift.line();
+
+    return buntstift;
+  },
+
+  list (message, { prefix = characters.multiplicationDot, indent = 0 } = {}) {
+    const width = indent * (prefix.length + 1);
+
+    prefix = new Array(width + 1).join(' ') + prefix;
+
+    buntstift.info(message, { prefix });
+
+    return buntstift;
+  },
+
+  newLine: decorators.skipIfQuiet(decorators.pauseSpinner(
+    () => {
+      console.log();
+
+      return buntstift;
+    }
+  )),
+
+  table (rows) {
+    if (!rows) {
+      throw new Error('Rows are missing.');
+    }
+
+    const widths = [];
+
+    rows.forEach(row => {
+      row.forEach((value, columnIndex) => {
+        widths[columnIndex] = Math.max(widths[columnIndex] || 0, String(value).length);
+      });
+    });
+
+    rows.forEach(row => {
+      const line = [];
+
+      if (row.length > 0) {
+        row.forEach((value, columnIndex) => {
+          line.push(pad(value, widths[columnIndex]));
+        });
+      } else {
+        widths.forEach(width => {
+          line.push(new Array(width + 1).join(characters.boxDrawingsLightHorizontal));
+        });
+      }
+
+      buntstift.info(line.join('  '));
+    });
+
+    return buntstift;
+  },
+
+  passThrough: decorators.pauseSpinner(
+    (message, { prefix = ' ', target = 'stdout' } = {}) => {
+      if (is.quiet() && target === 'stdout') {
+        return buntstift;
+      }
+
+      process[target].write(`${prefix || ' '} ${String(message)}`);
+
+      return buntstift;
+    }
+  ),
+
+  wait () {
+    if (is.quiet() || !is.interactiveMode()) {
+      return function () {
+        // Intentionally left blank.
+      };
+    }
+
+    if (stopSpinner) {
+      return;
+    }
+
+    const spinner = new Spinner();
+
+    interval = setInterval(() => {
+      process.stderr.write(`\r${spinner.next()}`);
+    }, 50);
+
+    stopSpinner = function () {
+      stopSpinner = undefined;
+      process.stderr.write('\r');
+      clearInterval(interval);
+    };
+
+    return stopSpinner;
+  },
+
+  exit (code = 0) {
+    if (stopSpinner) {
+      stopSpinner();
+    }
+
+    /* eslint-disable no-process-exit */
+    process.exit(code);
+    /* eslint-enable no-process-exit */
+  }
 };
 
 module.exports = buntstift;
--- src/unicode.js
+++ src/unicode.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const unicode = {};
+const unicode = {
+  utf8: {
+    checkMark: '\u2713',
+    crossMark: '\u2717',
+    multiplicationDot: '\u2219',
+    rightPointingPointer: '\u25bb',
+    boxDrawingsLightHorizontal: '\u2500'
+  },
 
-unicode.utf8 = {
-  checkMark: '\u2713',
-  crossMark: '\u2717',
-  multiplicationDot: '\u2219',
-  rightPointingPointer: '\u25bb',
-  boxDrawingsLightHorizontal: '\u2500'
-};
-
-unicode.ascii = {
-  checkMark: '+',
-  crossMark: '!',
-  multiplicationDot: '-',
-  rightPointingPointer: '>',
-  boxDrawingsLightHorizontal: '-'
+  ascii: {
+    checkMark: '+',
+    crossMark: '!',
+    multiplicationDot: '-',
+    rightPointingPointer: '>',
+    boxDrawingsLightHorizontal: '-'
+  }
 };
 
 module.exports = unicode;
